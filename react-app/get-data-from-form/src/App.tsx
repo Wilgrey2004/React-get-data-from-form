ecc74e0008cf9286a68de4b766205b025176ab0d
Wilgrey2004 <167430214+Wilgrey2004@users.noreply.github.com>
2024-05-23 19:58:26 -0500

    avance de hoy

---
+++ react-app/get-data-from-form/src/App.tsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+import { Form } from './Components/Form/Form'
+
+function App() {
+  return (
+    <div className='form'>
+      <Form nombreid='nombreus' apellidoid='apellidous' abuttonid='aceptarbtn' cbuttonid='cancelarbtn'></Form>
+    </div>
+  )
+}
+
+export default App
---
+++ react-app/get-data-from-form/src/Components/Form/Form.tsx
@@ -0,0 +1,23 @@
+import './Form.css'
+
+
+
+interface props {
+  nombreid: string,
+  apellidoid: string,
+  abuttonid : string,
+  cbuttonid: string
+}
+
+export const Form = ({nombreid,apellidoid,abuttonid,cbuttonid}:props) => {
+  return (
+    <div>
+      <form className="formulario" action="">
+        <input id = {nombreid} className="nombre" type="text" placeholder="Ingresa Tu nombre" />
+        <input id = {apellidoid} className="apellido" type="text" placeholder="Ingresa Tu Apellido" />
+        <button id = {abuttonid} className="aceptar">Aceptar</button>
+        <button id = {cbuttonid} className="cancelar">Cancelar</button>
+      </form>
+    </div>
+  );
+};
