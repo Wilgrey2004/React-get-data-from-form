2ba7f0b4a330a896ed21c7c602611b41c9f65100
Wilgrey2004 <167430214+Wilgrey2004@users.noreply.github.com>
2024-05-24 22:27:24 -0500

    Update Form.tsx

--- react-app/get-data-from-form/src/Components/Form/Form.tsx
+++ react-app/get-data-from-form/src/Components/Form/Form.tsx
@@ -1,22 +1,34 @@
-import './Form.css'
-
-
+import "./Form.css";
 
 interface props {
-  nombreid: string,
-  apellidoid: string,
-  abuttonid : string,
-  cbuttonid: string
+  nombreid: string;
+  apellidoid: string;
+  abuttonid: string;
+  cbuttonid: string;
 }
 
-export const Form = ({nombreid,apellidoid,abuttonid,cbuttonid}:props) => {
+export const Form = ({ nombreid, apellidoid, abuttonid, cbuttonid }: props) => {
   return (
     <div>
       <form className="formulario" action="">
-        <input id = {nombreid} className="nombre" type="text" placeholder="Ingresa Tu nombre" />
-        <input id = {apellidoid} className="apellido" type="text" placeholder="Ingresa Tu Apellido" />
-        <button id = {abuttonid} className="aceptar">Aceptar</button>
-        <button id = {cbuttonid} className="cancelar">Cancelar</button>
+        <input
+          id={nombreid}
+          className="nombre"
+          type="text"
+          placeholder="Ingresa Tu nombre"
+        />
+        <input
+          id={apellidoid}
+          className="apellido"
+          type="text"
+          placeholder="Ingresa Tu Apellido"
+        />
+        <button id={abuttonid} className="aceptar">
+          Aceptar
+        </button>
+        <button id={cbuttonid} className="cancelar">
+          Cancelar
+        </button>
       </form>
     </div>
   );
